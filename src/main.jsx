8604605a53c34b5cb9b5b38b8a864622b73aee53
Rahim00001 <rahimlpt2022@gmail.com>
2024-03-25 23:47:54 +0600

    tanstack query added on useRole hook

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,19 @@ import 'react-date-range/dist/theme/default.css';
 import AuthProvider from './providers/AuthProvider';
 import { Toaster } from 'react-hot-toast';
 import { HelmetProvider } from 'react-helmet-async';
+import {
+  QueryClient, QueryClientProvider
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
     <React.StrictMode>
       <AuthProvider>
-        <Toaster />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <Toaster />
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </AuthProvider>
     </React.StrictMode>
   </HelmetProvider>
