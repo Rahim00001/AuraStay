5d71c2d94913af698f937af6998efca45c892cea
Rahim00001 <rahimlpt2022@gmail.com>
2024-03-27 23:26:41 +0600

    user data row added on manage users page

--- src/pages/Dashboard/Admin/ManageUsers.jsx
+++ src/pages/Dashboard/Admin/ManageUsers.jsx
@@ -1,7 +1,17 @@
 
+import { useQuery } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
+import { getAllUsers } from '../../../api/auth'
+import UserDataRow from '../../../components/Dashboard/TableRows/UserDataRow'
 
 const ManageUsers = () => {
+    const { data: users = [],
+        //  refetch
+    } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => await getAllUsers(),
+    })
+    console.log(users)
     return (
         <>
             <div className='container mx-auto px-4 sm:px-8'>
@@ -41,7 +51,9 @@ const ManageUsers = () => {
                                         </th>
                                     </tr>
                                 </thead>
-                                <tbody>{/* User data table row */}</tbody>
+                                <tbody>
+                                    {users && users.map(user => <UserDataRow key={user._id} user={user}></UserDataRow>)}
+                                </tbody>
                             </table>
                         </div>
                     </div>
